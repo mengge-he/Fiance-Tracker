fe51559643724e9bdc7e2cd54fcf863b902dcb78
Mengge <hemengge00@gmail.com>
2026-05-08 00:03:31 -0400

    second commit: build some basic frame

---
+++ src/main/java/com/Mengge/finance_tracker/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.Mengge.finance_tracker.repository;
+
+import com.Mengge.finance_tracker.entity.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
